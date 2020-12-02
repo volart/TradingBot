894fb2e29ec324e2fc8744b188058e9794e8fb0d
volart <artem.volobuev@yandex.ru>
2020-12-02 11:46:26 +0300

    Init the project

---
+++ src/test/java/me/volart/auction/BidderTest.java
@@ -0,0 +1,14 @@
+package me.volart.auction;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BidderTest {
+  
+  @Test
+  public void test(){
+    assertTrue(true);
+  }
+  
+}
